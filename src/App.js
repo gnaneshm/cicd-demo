4b4ed36a06a962a8e9cc8177273ba6ede364eca4
gnaneshm <gnaneshm95@gmail.com>
2022-03-04 18:53:55 +0530

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{color:"red"}}>Welcome to RedPanda</h1>
+        <h1 style={{color:"red"}}>Welcome to Aknamed</h1>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
